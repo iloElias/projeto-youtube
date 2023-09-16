6eab8c6f144cfc4e1320d85072df014a4f97fc3b
iloElias <murilo7456@outlook.com>
2023-09-15 22:25:37 -0300

    Primeiro commit

---
+++ javascript/script.js
@@ -0,0 +1,69 @@
+let searchInputBar = document.getElementById("main-search-input")
+let originalSearchInputBarColor = document.querySelector("div.search-input-border").style.borderColor
+let canShow = true
+
+document.getElementById("youtube-search-icon").addEventListener("animationend", function () {
+    if (canShow) {
+        document.getElementById("youtube-search-icon").style.display = "flex"
+        canShow = false
+    } else {
+        document.getElementById("youtube-search-icon").style.display = "none"
+        canShow = true
+    }
+    return
+});
+
+searchInputBar.addEventListener("focus", function () {
+    let mainInputBorder = document.getElementById("search-input-border")
+    let searchIcon = document.getElementById("youtube-search-icon")
+
+    document.getElementById("spaceBtw").style.animation = "spaceShow 0.1s"
+    document.getElementById("spaceBtw").addEventListener("animationend", function () {
+        document.getElementById("spaceBtw").style.width = "0px"
+        searchIcon.style.display = "block"
+        searchIcon.style.display = "flex"
+    })
+
+    document.getElementById("search-input").style.animation = "showAnimation 0.1s"
+    document.getElementById("search-input").style.margin = "0 0 0 0px";
+
+    mainInputBorder.style.borderColor = "#1c62b9"
+})
+
+searchInputBar.addEventListener("blur", function () {
+    let mainInputBorder = document.getElementById("search-input-border")
+    let searchIcon = document.getElementById("youtube-search-icon")
+
+    searchIcon.style.display = "none"
+    document.getElementById("spaceBtw").style.width = "36px"
+    document.getElementById("spaceBtw").style.animation = "spaceHide 0.1s"
+    document.getElementById("spaceBtw").addEventListener("animationend", function () {
+        searchIcon.style.display = "none"
+    })
+
+    document.getElementById("search-input").style.animation = "hideAnimation 0.1s"
+    document.getElementById("search-input").style.margin = "0 0 0 32px";
+
+    mainInputBorder.style.borderColor = originalSearchInputBarColor
+})
+
+searchInputBar.addEventListener("input", function (selected) {
+    if (selected.target.value == "") {
+        document.getElementById("remove-all-search").style.display = "none"
+        document.getElementById("digital-keyboard").style.right = "0"
+    } else {
+        document.getElementById("remove-all-search").style.display = "flex"
+        document.getElementById("digital-keyboard").style.right = "-16px"
+    }
+})
+
+document.getElementById("remove-all-search").addEventListener("click", function () {
+    if (searchInputBar.value != "") {
+        searchInputBar.value = ""
+        document.getElementById("remove-all-search").style.display = "none"
+        document.getElementById("digital-keyboard").style.right = "0"
+    }
+})
+
+
+
